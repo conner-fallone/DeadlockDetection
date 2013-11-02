95d155491c54918ef6444f6887ca7af08b4e4b8b
Conner Fallone <conner4892@gmail.com>
2013-11-02 16:53:49 -0400

    Piped deadlock detector now working.

--- pipedDetector.c
+++ pipedDetector.c
@@ -1,3 +1,9 @@
+/******************************************************
+ * Author: Conner Fallone
+ * CS-452 Fall 2013
+ * Deadlock Detection
+ ******************************************************/
+
 #include <stdio.h> 
 #include <stdlib.h>
 #include <pthread.h> 
@@ -6,38 +12,26 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <unistd.h>
 
-#define SIZE 50
-#define MAX_BUF 1024
-
-typedef struct probeStruct{
-	// BlockedProcess : Sender : Receiver
-	char *blockedProcess;
-	char *senderProcess;
-	char *receiverProcess;
-} Probe;
-
-Probe probe;
-Probe temp;
 pthread_t sender;
 pthread_t receiver; 
 char line[100];
 char *owners[20];
 char *requesters[20];
+char *probes[3];
 char *token;
 char *procNum;
 char *resource;
 char *resourceOwner;
-int i,status,counter,counter2,fd;
+int i,status,ownersCounter,requesterCounter,fd;
 bool isBlocked = false;
 bool isDeadlocked = false;
 void *senderThread();
 void *receiverThread();
 void findOwner();
 
-char buf[MAX_BUF];
-
-int main(int argc, char *argv[]) 
+int main(int argc, char *argv[])
 {
 	// Check arguments -> Display usage if incorrect arguments
 	if (argc != 3){	
@@ -47,7 +41,6 @@ int main(int argc, char *argv[])
 	else{
 		// Get process number argument
 		procNum = argv[2];
-
 		// Open configuration file
 		FILE *file = fopen(argv[1],"r");
 		if (file == 0){
@@ -56,7 +49,7 @@ int main(int argc, char *argv[])
 			// Opened file successfully.	
 			// Store configuration file
 			while (fgets(line,100,file) != NULL){
-				printf("Line: %s\n",line);
+				printf("Parsing Line: %s\n",line);
 				// If the line read contains owns, store it in owners array
 				if (strstr(line,"owns") != NULL){
 					
@@ -68,10 +61,10 @@ int main(int argc, char *argv[])
 							continue;
 						}
 						// Allocate space and copy token into array
-						owners[counter] = malloc(strlen(token) + 1);
-						strcpy(owners[counter], token);
+						owners[ownersCounter] = malloc(strlen(token) + 1);
+						strcpy(owners[ownersCounter], token);
 						token = strtok(NULL, " ");
-						counter++;
+						ownersCounter++;
 					}
 				}else{
 					// We have a request line, see if it is my own proces, block if it is
@@ -85,13 +78,12 @@ int main(int argc, char *argv[])
 								continue;
 							}
 							// Allocate space and copy token into array
-							requesters[counter2] = malloc(strlen(token) + 1);
-							strcpy(requesters[counter2], token);
+							requesters[requesterCounter] = malloc(strlen(token) + 1);
+							strcpy(requesters[requesterCounter], token);
 							token = strtok(NULL, " ");
-							counter2++;
+							requesterCounter++;
 						}	
-						isBlocked = true;	
-						printf("***** %s is blocking *****\n",procNum);
+						isBlocked = true;		
 					}
 
 				}
@@ -106,75 +98,90 @@ int main(int argc, char *argv[])
 		}	
 	}
 	// Create sender thread
-	if ((status = pthread_create(&sender,NULL,senderThread,"Testing create")) != 0){
+	if ((status = pthread_create(&sender,NULL,senderThread,NULL)) != 0){
 		fprintf (stderr, "Error creating thread - Status: %d: %s\n", status, strerror(status));
 		exit (1);
 	}
 
 	// Create receiver thread
-	if ((status = pthread_create(&receiver,NULL,receiverThread,"Testing create")) != 0){
+	if ((status = pthread_create(&receiver,NULL,receiverThread,NULL)) != 0){
 		fprintf (stderr, "Error creating thread - Status: %d: %s\n", status, strerror(status));
 		exit (1);
 	}
 
 	// Main thread - loop while not deadlocked
 	while (!isDeadlocked) {
-		//printf("Looping\n");
-		sleep(1);
+		// do nothing while not deadlocked	
 	}
 
-	printf("**** System is deadlocked ****");
+	printf("**** System is deadlocked ****\n");
 return 1;
 }
 
-void *senderThread(void *arg){
+/*******************************************************
+ * Sending thread, sends probes every 10 seconds.
+ *******************************************************/
+void *senderThread(){
 	while (isBlocked){
-		// Process is blocked
 		// Sends a Probe to the process owning the resource it is blocked on
 		fd = open(resourceOwner, O_WRONLY);
-		printf("Writing %s:%s:%s\n",probe.blockedProcess, probe.senderProcess, probe.receiverProcess);
-		write(fd, probe, sizeof(Probe));
-		printf("%s wrote to the pipe\n",procNum);
-		//close(fd);
-		//unlink(resourceOwner);
+		printf("%s is Writing %s:%s:%s\n",procNum,probes[0], probes[1], probes[2]);	
+		write(fd, probes[0], sizeof(char) * 2);
+		write(fd, probes[1], sizeof(char) * 2);
+		write(fd, probes[2], sizeof(char) * 2);
+		close(fd);
+		printf("%s wrote to the pipe.\n",procNum);
 		sleep(10);
-		printf("**** Sending probe: %s:%s:%s ****\n", probe.blockedProcess, probe.senderProcess, probe.receiverProcess);
 	}
-}
-void *receiverThread(void *arg){
-	while (1){
-		// Listen for probe - Look for any pipe with a name equivalent to process number
-		// this means there is a process that wishes to contact us.
-		if (fd = open(procNum, O_RDONLY) < 0){
-			printf("I am %s and noone is trying to contact me.\n",procNum);
-		}else{
-			printf("I am %s and someone is trying to contact me.\n",procNum);
-			//read(fd, buf, MAX_BUF);
-			printf("Attempting to read from pipe\n");
-			read(fd,temp,sizeof(Probe));
-			printf("Read from pipe\n");
-			printf("I am %s and I just read: %s:%s:%s\n",procNum,temp.blockedProcess, temp.senderProcess, temp.receiverProcess);
-		}
-		// Check probe 1st and 3rd spots. If equal, deadlocked = true
-		if (isBlocked){
-			// Struct current probe = probe read
-			// findOwner();
-			//modifies the Sender and Receiver fields and forwards the message to the process owning the resource it is blocked on. 	
-		}else{
-			probe.blockedProcess = NULL;
-			probe.senderProcess = NULL;
-			probe.receiverProcess = NULL;
-		}
-		printf("Testing receiver: %s\n", (char*)arg);
-		sleep(5);
-	}
+return NULL;
 }
 
+/*******************************************************
+ * Receiving thread, constantly looking for a probe
+ * that wants to contact the process.
+ *******************************************************/
+void *receiverThread(){
+	while (1){
+		// Listen for probe - Look for any pipe with a name equivalent to process number
+		// this means there is a process that wishes to contact me
+		fd = open(procNum, O_RDONLY);
+		if (fd < 0){
+			// There is no pipe with my name. Noone wants to contact me
+		}else{
+			if (isBlocked){
+				read(fd,probes[0],sizeof(char) * 2);
+				read(fd,probes[1],sizeof(char) * 2);
+				read(fd,probes[2],sizeof(char) * 2);
+				printf("%s has read %s:%s:%s\n",procNum,probes[0],probes[1],probes[2]);
+				// Check probe blocked and receiving process. If same = deadlocked
+				if (!strcmp(probes[0],probes[2])){
+					printf("%s detected deadlock because %s = %s\n",procNum,probes[0],probes[2]);
+					close (fd);
+					unlink (procNum);
+					free(requesters[0]);
+					isDeadlocked = true;
+				}		
+				strcpy(probes[1],procNum);
+				strcpy(probes[2],resourceOwner);
+			}else{
+				printf("%s is not blocked and received a probe. discarding.\n",procNum);
+			}
+		}
+	sleep(5);	
+	}
+return NULL;
+}
+
+/*******************************************************
+ * Called when the process is blocking. Find the owner
+ * of the resource the process is blocked on, and forms
+ * the probe.
+ *******************************************************/
 void findOwner(){
+	printf("***** %s is blocking *****\n",procNum);
 	// Search through requesters. Find the resource i'm blocked on
 	while (1){
 		if (!strcmp(requesters[i],procNum)){
-			printf("%s wants %s\n",requesters[i],requesters[i+1]);
 			resource = requesters[i+1];
 			break;
 		}
@@ -184,7 +191,6 @@ void findOwner(){
 	// Search through owners. Find the owner of the resource i'm blocked on
 	while (1){
 		if (!strcmp(owners[i],resource)){
-			printf("%s owns %s\n",owners[i-1],owners[i]);
 			resourceOwner = owners[i-1];
 			break;
 		}
@@ -192,10 +198,13 @@ void findOwner(){
 	}
 	// Create pipe/file
 	mkfifo(resourceOwner,0666);
-	printf("pipe created\n");	
-	// Form probe
-	probe.blockedProcess = procNum;	
-	probe.senderProcess = procNum;
-	probe.receiverProcess = resourceOwner;
+	printf("%s created the pipe %s\n",procNum,resourceOwner);	
+	// Form probe for blocked process
+	probes[0] = malloc(strlen(procNum) + 1);
+	strcpy(probes[0], procNum);
+	probes[1] = malloc(strlen(procNum) +1);
+	strcpy(probes[1], procNum);
+	probes[2] = malloc(strlen(resourceOwner) + 1);
+	strcpy(probes[2], resourceOwner);
 	i = 0;
 }
